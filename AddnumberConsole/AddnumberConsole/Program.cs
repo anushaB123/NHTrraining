2d03d86a4e5fd718ff058165e14fcdd46f3cce1f
anushaB123 <Anusha2010.it@gmail.com>
2022-01-25 18:39:51 +0530

    first submission

---
+++ AddnumberConsole/AddnumberConsole/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddnumberConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int fn, sn, sum = 0;
+            Console.WriteLine("Enter First Number:");
+            fn = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Second Number:");
+            sn = Convert.ToInt32(Console.ReadLine());
+            sum = fn + sn;
+            Console.WriteLine("sum=" + sum);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ powernumConsole/powernumConsole/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace powernumConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int pow = 1,fn,sn;
+            Console.WriteLine("Enter first number:");
+            fn = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second number:");
+            sn = Convert.ToInt32(Console.ReadLine());
+            for (int i=1;i<=sn;i++)
+            {
+                pow = pow * fn;
+            }
+            Console.WriteLine("p="+pow);
+            Console.ReadLine();
+        }
+    }
+}
