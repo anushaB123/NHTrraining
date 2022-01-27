ba19ac0f5ee520392a32317700191a370411ee40
anushaB123 <Anusha2010.it@gmail.com>
2022-01-27 22:21:30 +0530

    sixth assignment

---
+++ FactorialusingFunctions/FactorialusingFunctions/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactorialusingFunctions
+{
+    internal class Program
+    {
+        //purpose to read a number and print its factorial
+        int Factorial(int input);
+
+        static void Main(string[] args)
+        {
+            //variable declaration
+            int fn, sn;
+
+
+            //read the data from user
+            Console.WriteLine("enter first number:");
+            fn = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("enter second number:");
+            sn = Convert.ToInt32(Console.ReadLine());
+
+            //print the output
+            Console.WriteLine("Factorial of {0} is {1}", fn, Factorial(fn));
+            Console.WriteLine("Factorial of {0} is {1}", sn, Factorial(sn));
+            Console.ReadLine();
+
+            int Factorial(int input)
+            {
+
+                int fact = 1;
+                for (int i = 0; i < input; i++)
+                {
+                    fact = fact * i;
+                    return fact;
+                }
+
+            }
+        }
+        
+    }
+}
+
+
+
+   
+
