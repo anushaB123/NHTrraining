ee8003610c5c20fb44be939a318c0f08b1218a7b
anushaB123 <Anusha2010.it@gmail.com>
2022-02-03 20:09:34 +0530

    ppt

---
+++ Day 7 Morning Assignment/Bubblesort/Bubblesort/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bubblesort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] arr = { 5, 1, 4, 2, 8 };
+            int temp;
+            for (int j = 0; j <= arr.Length - 2; j++)
+            {
+                for (int i = 0; i <= arr.Length - 2; i++)
+                {
+                    if (arr[i] > arr[i + 1])
+                    {
+                        temp = arr[i + 1];
+                        arr[i + 1] = arr[i];
+                        arr[i] = temp;
+                    }
+                }
+            }
+            Console.WriteLine("Sorted:");
+            foreach (int p in arr)
+                Console.Write(p + " ");
+            Console.Read();
+        }
+    }
+}
---
+++ Day 7 Morning Assignment/Linearsearch/Linearsearch/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linearsearch
+{
+    class Searching
+    {
+        public static int search(int[] arr, int x)
+        {
+            int n = arr.Length;
+            for (int i = 0; i < n; i++)
+            {
+                if (arr[i] == x)
+                    return i;
+            }
+            return -1;
+        }
+        internal class Program
+        {
+       
+            static void Main(string[] args)
+             {
+                int[] arr = { 10, 50, 30, 70, 80, 60, 20, 90, 40 };
+                int x = 20;
+
+                // Function call
+                int result = search(arr, x);
+                if (result == -1)
+                    Console.WriteLine("Element is not present in array");
+                else
+                    Console.WriteLine("Element is present at index " + result);
+                Console.ReadLine();
+            }
+        }
+    }
+}
