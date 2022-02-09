00cbd67f47f8bf8effea85753ae1b26c65ca3937
anushaB123 <Anusha2010.it@gmail.com>
2022-02-09 19:59:32 +0530

    day12assignment

---
+++ Day12 Morning Assignment/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        public static object ViewBag { get; private set; }
+
+        static void Main(string[] args)
+        {
+
+            StringBuilder stringBuilder = new StringBuilder(17, 17);
+            stringBuilder.Append("Welcome to the ");
+            
+                stringBuilder.Insert(0, "world of C# programming", 1);
+                Console.WriteLine(stringBuilder.ToString());
+                Console.ReadLine();
+            
+            
+        }
+    }
+}
+
+    
+
---
+++ Day12 Morning Assignment/Day12_Morning/Day12_Morning/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day12_Morning
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int a, b, c;
+                Console.WriteLine("Enter first number:");
+                a = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter second number:");
+                b = Convert.ToInt32(Console.ReadLine());
+                c = a / b;
+              
+                Console.WriteLine("Answer={0}", c);
+              
+                Console.ReadLine();
+            }
+            catch(OverflowException)
+            {
+                Console.WriteLine("number should be 0 to 50000 are allowed");
+            }
+            catch(DivideByZeroException)
+            {
+                Console.WriteLine("cannot divide by zero");
+            }
+            catch(FormatException)
+            {
+                Console.WriteLine("only numbers are allowed. please double check");
+            }
+            catch(Exception)
+            {
+                Console.WriteLine("Any errors will occur contact admin@mycompany.com");
+                Console.ReadLine();
+            }
+            
+        }
+    }
+}
