7c12b0a3b969a1915cc667d89b73aeb7919f1cce
anushaB123 <Anusha2010.it@gmail.com>
2022-02-15 21:03:19 +0530

    day17 assignment

---
+++ Day17MorningAssignment/Day17Project1/AnuLibrary/MyBaseClass.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnuLibrary
+{
+    public class MyBaseClass
+    {
+        public int a;
+        private int b;
+        protected int c;
+        internal int d;
+        protected internal int e;
+
+        public void MyBaseClassMethod()
+        {
+            a = 5;
+            b = 10;
+            c = 15;
+            d = 20;
+            e = 30;
+
+        }
+
+        public class MyDerivedClass : MyBaseClass
+        {
+            public void MyDerivedClassMethod()
+            {
+                a = 5;
+                // b = 10;	// Here the private Access Modifier	is not accessed	in the Derived  Class
+                c = 15;
+                d = 20;
+                e = 30;
+
+
+            }
+        }
+        public class MyOtherClass
+        {
+            public void MyOtherClassMethod()
+            {
+                MyBaseClass mb = new MyBaseClass();
+                mb.a = 5;
+                //  mb.b  =  10;  Here  the  private  Access  Modifier	is  not  accessed	in  the  other Class	in  Same  Assembly
+                //  mb.c  =  15;  Here  the  protected  Access  Modifier	is  not  accessed	in  the  other Class	in  Same  Assembly
+                mb.d = 20;
+                mb.e = 30;
+
+
+            }
+        }
+
+    }
+}
+
---
+++ Day17MorningAssignment/Day17Project1/PublicLibrary1/MyPublicLibraryDerivedClass.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AnuLibrary;
+
+namespace PublicLibrary1
+{
+    internal class MyPublicLibraryDerivedClass : MyBaseClass
+    {
+        public void MypublicLibraryDerivedClassMethod()
+        {
+            a = 5;
+            //b = 10;// Here the private Access Modifier is not accessed in the Derived Class in  Other  Assembly
+            c = 15;
+            //d = 20;// Here the internal Access Modifier is not accessed in the Derived  Class in  Other  Assembly
+            e = 30;
+        }
+
+
+    }
+
+
+    public class MyPublicLibraryOtherClass
+    {
+        public void MypublicLibraryOtherClassMethod()
+        {
+            MyBaseClass mpd = new MyBaseClass();
+            mpd.a = 5;
+            //  mpd.b  =  10;\\Here  the  private  Access  Modifier is  not  accessed in  the  other Class in  Other  Assembly
+            //  mpd.c  =  15;\\Here  the  protected  Access  Modifier is  not  accessed	in  the  other Class in  Other  Assembly
+            //  mpd.d  =  20;\\Here  the internal  Access  Modifier is  not  accessed in  the  other Class in  Other  Assembly
+            //  mpd.e  =  30;\\Here  the  protected  Internal  Access  Modifier	is  not  accessed in the  other  Class in  Other  Assembly
+        }
+
+    }
+
+}
+
